28d81e8e205da44a78a0336a4b65beae0d933fd8
suetAndTie <trtets@gmail.com>
2019-06-04 23:19:12 -0700

    prepare config

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
 
     now = datetime.datetime.now(dateutil.tz.tzlocal())
     timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
-    output_dir = '../output/%s_%s_%s' % \
+    output_dir = '/output/%s_%s_%s' % \
         (cfg.DATASET_NAME, cfg.CONFIG_NAME, timestamp)
 
     split_dir, bshuffle = 'train', True
