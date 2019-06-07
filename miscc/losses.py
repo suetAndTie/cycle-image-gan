cb1e332874fd9c7171fc486fa7eba7d7e748b010
suetAndTie <trtets@gmail.com>
2019-06-07 05:07:01 -0700

    fixed loss shape

--- miscc/losses.py
+++ miscc/losses.py
@@ -9,6 +9,11 @@ from GlobalAttention import func_attention
 
 ####################Loss for image2text ###################
 def image_to_text_loss(output, target):
+    # bs x T x vocab_size - > bs * T x vocab_size
+    bs, T, vocab_size = output.shape
+    output = output.view(-1, vocab_size)
+    # bs x T -> bs * T
+    target = target.view(-1)
     return F.cross_entropy(output, target)
 
 
